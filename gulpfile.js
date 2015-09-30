42ce579f2b43570daa640c607eb815261bcb91dc
Richard Olsson <r@richardolsson.se>
2015-09-30 18:26:17 +0200

    Add watch gulp task to build and restart dev server

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,24 @@
 var babel = require('gulp-babel');
 var gulp = require('gulp');
+var shell = require('gulp-shell');
+
 
 gulp.task('default', function() {
     gulp.src('src/**/*.@(js|jsx)')
         .pipe(babel())
         .pipe(gulp.dest('./dist/app'));
+});
+
+gulp.task('restartDevServer', shell.task([
+    'docker exec env_app_1 sv restart app'
+]));
+
+gulp.task('watch', function() {
+    return gulp.watch('src/**/*.@(js|jsx)', ['restartDevServer'])
+        .on('change', function(ev) {
+            console.log(ev.path);
+            return gulp.src(ev.path)
+                .pipe(babel())
+                .pipe(gulp.dest('./dist/app'));
+        });
 });
